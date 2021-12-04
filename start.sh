993c7613e5226c64244345a795d30a48ca58b342
OskiBoski483 <79474077+OskiBoski483@users.noreply.github.com>
2021-12-04 13:40:02 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,8 @@
 #!/bin/ash
 
-echo "Starting PHP-FPM..."
+
 /usr/sbin/php-fpm8 --fpm-config /home/container/php-fpm/php-fpm.conf --daemonize
 
-echo "Starting Nginx..."
+echo "Uruchamianie strony www..."
 /usr/sbin/nginx -c /home/container/nginx/nginx.conf
+echo "Gotowe!"
