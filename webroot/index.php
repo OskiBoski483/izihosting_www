6e4d992304ae7bb3a1ead56bc992379e90186ed9
OskiBoski483 <79474077+OskiBoski483@users.noreply.github.com>
2021-12-04 13:20:58 +0100

    Update index.php

--- webroot/index.php
+++ webroot/index.php
@@ -1,3 +1,13 @@
-<?php
-echo "Hello World!";
-phpinfo();
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>IziHosting</h1><br>
+    <span>Domyślna strona :) podmień ten plik na swoją strone</span>
+
+  </body>
+</html>
+
