df30b4d5fac05bc0aae479d64ec2dc654951557b
OskiBoski483 <79474077+OskiBoski483@users.noreply.github.com>
2021-12-23 12:03:46 +0100

    Delete start.sh

--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/ash
-
-
-/usr/sbin/php-fpm8 --fpm-config /home/container/php-fpm/php-fpm.conf --daemonize
-
-echo "Uruchamianie strony www..."
-/usr/sbin/nginx -c /home/container/nginx/nginx.conf
-echo "Gotowe!"
